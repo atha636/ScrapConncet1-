5de4b8144d0b8a9d2e1bc0a02d684db9e788f978
Atharv Patidar <atharvpatidar2005@gmail.com>
2026-07-13 08:52:11 +0530

    starting error boundry

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext"; // 👈 add this
-
+import { AuthProvider } from "./context/AuthContext"; 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
